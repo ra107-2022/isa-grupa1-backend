27246f13fcd15e4138919caa03383c92455cfa48
Nikolina Aleksic <aleksic.ra38.2022@uns.ac.rs>
2026-01-30 03:32:17 +0100

    fix: ispravak greske

--- jutjubic/src/main/java/grupa1/jutjubic/JutjubicApplication.java
+++ jutjubic/src/main/java/grupa1/jutjubic/JutjubicApplication.java
@@ -3,7 +3,9 @@ package grupa1.jutjubic;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -13,6 +15,11 @@ public class JutjubicApplication {
 	@Bean
 	public ModelMapper getModelMapper() {
 		return new ModelMapper();
+	}
+
+	@Bean
+	public CacheManager cacheManager() {
+		return new ConcurrentMapCacheManager("videoComments");
 	}
 
 	public static void main(String[] args) {
