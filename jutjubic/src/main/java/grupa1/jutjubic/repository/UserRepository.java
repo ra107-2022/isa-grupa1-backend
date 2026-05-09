c284868404768548fcebe0afa503e0a964fe9be7
Luka Kojović <kojovic.ra107.2022@uns.ac.rs>
2026-05-09 03:55:16 +0200

    feat: Added docker-compose file for running Prometheus and Grafana. Made a Grafana Dashboard for monitoring all relevant metrics.

--- jutjubic/src/main/java/grupa1/jutjubic/repository/UserRepository.java
+++ jutjubic/src/main/java/grupa1/jutjubic/repository/UserRepository.java
@@ -3,7 +3,10 @@ package grupa1.jutjubic.repository;
 import grupa1.jutjubic.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.Instant;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
     User findByUsername(String username);
+    int countByLastActiveAtAfter(Instant time);
 }
